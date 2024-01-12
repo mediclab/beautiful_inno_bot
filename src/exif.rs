d2325aa56d86479f3763dc75893eea5171c619f4
Dmitry Kozhanov <Medic8463@gmail.com>
2024-01-12 13:02:47 +0300

    Add photo info string (#3)

--- src/exif.rs
+++ src/exif.rs
@@ -17,28 +17,79 @@ impl ExifLoader {
     }
 
     pub fn get_maker(&self) -> String {
-        if let Some(make_field) = self.exif.get_field(Tag::Make, In::PRIMARY) {
-            return match make_field.value {
-                Value::Ascii(ref v) if !v.is_empty() => {
-                    make_field.display_value().to_string().replace('"', "")
-                }
-                _ => String::new(),
-            };
+        if let Some(field) = self.get_field_string(&Tag::Make) {
+            return field;
         }
 
         String::new()
     }
 
     pub fn get_model(&self) -> String {
-        if let Some(model_field) = self.exif.get_field(Tag::Model, In::PRIMARY) {
-            return match model_field.value {
-                Value::Ascii(ref v) if !v.is_empty() => {
-                    model_field.display_value().to_string().replace('"', "")
-                }
-                _ => String::new(),
-            };
+        if let Some(field) = self.get_field_string(&Tag::Model) {
+            return field;
         }
 
         String::new()
+    }
+
+    pub fn get_exposure_time(&self) -> String {
+        if let Some(field) = self.get_field_string(&Tag::ExposureTime) {
+            return format!("{}s", field);
+        }
+
+        String::new()
+    }
+
+    pub fn get_focal_number(&self) -> String {
+        if let Some(field) = self.get_field_string(&Tag::FNumber) {
+            return format!("f/{}", field);
+        }
+
+        String::new()
+    }
+
+    pub fn get_focal_length(&self) -> String {
+        if let Some(field) = self.get_field_string(&Tag::FocalLength) {
+            return format!("{}mm", field);
+        }
+
+        String::new()
+    }
+
+    pub fn get_iso(&self) -> String {
+        if let Some(field) = self.get_field_string(&Tag::PhotographicSensitivity) {
+            return format!("ISO{}", field);
+        }
+
+        String::new()
+    }
+
+    pub fn get_photo_info_string(&self) -> String {
+        format!(
+            "{} {} {} {}",
+            self.get_focal_number(),
+            self.get_exposure_time(),
+            self.get_focal_length(),
+            self.get_iso()
+        )
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join(" ")
+    }
+
+    fn get_field_string(&self, tag: &Tag) -> Option<String> {
+        if let Some(field) = self.exif.get_field(*tag, In::PRIMARY) {
+            debug!("{} field: {:?}", field.tag, field.value);
+            return match field.value {
+                Value::Rational(ref v) if !v.is_empty() => Some(field.display_value().to_string()),
+                Value::Ascii(ref v) if !v.is_empty() => {
+                    Some(field.display_value().to_string().replace('"', ""))
+                }
+                Value::Short(ref v) if !v.is_empty() => Some(field.display_value().to_string()),
+                _ => None,
+            };
+        }
+
+        None
     }
 }
