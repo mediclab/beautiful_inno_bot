0557d9e76a766f71d8fcca4c5148a62a12b1e6dc
Dmitriy Kozhanov <m@mdlb.cc>
2025-04-02 21:12:59 +0000

    [BEIB-1] Add saving reactions
    Changelog: features

--- src/db/entity/prelude.rs
+++ src/db/entity/prelude.rs
@@ -2,4 +2,5 @@
 
 pub use super::ban::Entity as Ban;
 pub use super::photos::Entity as Photos;
+pub use super::reactions::Entity as Reactions;
 pub use super::users::Entity as Users;
--- src/db/types.rs
+++ src/db/types.rs
@@ -1,5 +1,6 @@
+use crate::db::entity::reactions::ReactionType;
 use sea_orm::Set;
-use teloxide::types::{Message, User};
+use teloxide::types::{Message, ReactionCount, ReactionType as TgReactionType, User};
 
 impl From<User> for super::entity::users::ActiveModel {
     fn from(value: User) -> Self {
@@ -22,6 +23,48 @@ impl From<Message> for super::entity::photos::ActiveModel {
             file_id: Set(doc.file.id.clone()),
             mime_type: Set(doc.mime_type.clone().map(|u| u.to_string())),
             ..Default::default()
+        }
+    }
+}
+
+pub struct Reactions {
+    pub r#type: ReactionType,
+    pub content: Option<String>,
+    pub count: u64,
+}
+
+impl From<&ReactionCount> for Reactions {
+    fn from(react: &ReactionCount) -> Self {
+        match &react.r#type {
+            TgReactionType::Emoji { emoji } => Reactions {
+                r#type: ReactionType::Emoji,
+                content: Some(emoji.clone()),
+                count: react.total_count,
+            },
+            TgReactionType::CustomEmoji { custom_emoji_id } => Reactions {
+                r#type: ReactionType::Emoji,
+                content: Some(custom_emoji_id.clone()),
+                count: react.total_count,
+            },
+            TgReactionType::Paid => Reactions {
+                r#type: ReactionType::Paid,
+                content: None,
+                count: react.total_count,
+            },
+        }
+    }
+}
+
+impl From<super::entity::reactions::Model> for TgReactionType {
+    fn from(value: super::entity::reactions::Model) -> Self {
+        match value.r#type {
+            ReactionType::Emoji => TgReactionType::Emoji {
+                emoji: value.content.unwrap(),
+            },
+            ReactionType::CustomEmoji => TgReactionType::CustomEmoji {
+                custom_emoji_id: value.content.unwrap(),
+            },
+            ReactionType::Paid => TgReactionType::Paid,
         }
     }
 }
